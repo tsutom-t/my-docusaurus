39c946231a5b00b72498f6cd8da6b4d3025de36e
tsutom-t <takatop162@gmail.com>
2024-11-17 16:10:13 +0900

    add-tailwindcss

---
+++ docs/hello.md
@@ -0,0 +1,12 @@
+---
+sidebar_label: "TomT-Plus!"
+sidebar_position: 4
+---
+
+# Hello
+
+This is my **first Docusaurus document**!
+
+# Hello
+
+This is my **first Docusaurus document**!
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,33 +1,87 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
+import path from "path";
 
 const config: Config = {
-  title: "My Site",
+  title: "TomT-Plus",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: " https://tsutom-t.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  url: "https://tsutom-t.github.io",
   baseUrl: "/my-docusaurus",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: "tsutom-t", // Usually your GitHub org/user name.
-  projectName: "my-docusaurus", // Usually your repo name.
+  organizationName: "tsutom-t",
+  projectName: "my-docusaurus",
   trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "ja",
+    locales: ["ja"],
+  },
+
+  plugins: [
+    async function tailwindPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
+
+  customFields: {
+    webpack: {
+      jsLoader: (isServer) => ({
+        loader: require.resolve("swc-loader"),
+        options: {
+          jsc: {
+            parser: {
+              syntax: "typescript",
+              tsx: true,
+            },
+            target: "es2017",
+          },
+          module: {
+            type: isServer ? "commonjs" : "es6",
+          },
+        },
+      }),
+
+      configure: (config, isServer, utils) => {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          "@": path.resolve(__dirname, "./src"),
+        };
+
+        config.module.rules.push({
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: require.resolve("swc-loader"),
+              options: {
+                jsc: {
+                  parser: {
+                    syntax: "typescript",
+                    tsx: true,
+                  },
+                  target: "es2017",
+                },
+              },
+            },
+          ],
+        });
+
+        return config;
+      },
+    },
   },
 
   presets: [
@@ -36,8 +90,6 @@ const config: Config = {
       {
         docs: {
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
@@ -47,13 +99,10 @@ const config: Config = {
             type: ["rss", "atom"],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
-          // Useful options to enforce blogging best practices
           onInlineTags: "warn",
-          onInlineAuthors: "warn",
+          onInlineAuthors: "ignore",
           onUntruncatedBlogPosts: "warn",
         },
         theme: {
@@ -64,40 +113,28 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "TsutomuT-Plus",
+      title: "TomT-Plus",
       logo: {
-        alt: "My Site Logo",
+        alt: "Site Logo",
         src: "img/logo.svg",
       },
       items: [
         {
-          type: "docSidebar",
-          sidebarId: "tutorialSidebar",
+          to: "/docs/intro",
+          label: "Docs",
           position: "left",
-          label: "Tutorial",
         },
-        { to: "/blog", label: "Blog", position: "left" },
-
+        {
+          to: "/blog",
+          label: "Blog",
+          position: "left",
+        },
         {
           href: "https://github.com/tsutom-t/my-docusaurus",
           position: "right",
-          className: "navbar-github-link",
+          className: "header-github-icon",
           "aria-label": "GitHub repository",
-          // labelを削除
-          html: `
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-            >
-              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-            </svg>
-          `,
         },
       ],
     },
@@ -108,7 +145,7 @@ const config: Config = {
           title: "Docs",
           items: [
             {
-              label: "Tutorial",
+              label: "Getting Started",
               to: "/docs/intro",
             },
           ],
@@ -117,16 +154,16 @@ const config: Config = {
           title: "Community",
           items: [
             {
-              label: "Stack Overflow",
-              href: "https://stackoverflow.com/questions/tagged/docusaurus",
-            },
-            {
-              label: "Discord",
-              href: "https://discordapp.com/invite/docusaurus",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/docusaurus",
+              html: `
+            <a
+              class="footer-github-icon"
+              href="https://github.com/tsutom-t/my-docusaurus"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="GitHub repository"
+            >
+            </a>
+          `,
             },
           ],
         },
@@ -137,18 +174,10 @@ const config: Config = {
               label: "Blog",
               to: "/blog",
             },
-            {
-              label: "GitHub",
-              href: "https://github.com/facebook/docusaurus",
-            },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} TsutomT, Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
+      copyright: `Copyright © ${new Date().getFullYear()} My Site, Inc. Built with Docusaurus.`,
     },
   } satisfies Preset.ThemeConfig,
 };
