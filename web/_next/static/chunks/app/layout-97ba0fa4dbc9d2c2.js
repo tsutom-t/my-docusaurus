1a020e0e67e2b82fa9efcb915d7e989e6af2059f
tsutom-t <takatop162@gmail.com>
2024-12-21 23:50:16 +0000

    deploy: 5600a5157eda37c53f7499960844432bf2c63b85

---
+++ web/_next/static/chunks/app/layout-97ba0fa4dbc9d2c2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{2606:function(n,a,e){Promise.resolve().then(e.t.bind(e,2740,23))},2740:function(n){n.exports={style:{fontFamily:"'__Inter_da74ad', '__Inter_Fallback_da74ad'",fontStyle:"normal"},className:"__className_da74ad"}}},function(n){n.O(0,[622,293,528,744],function(){return n(n.s=2606)}),_N_E=n.O()}]);
